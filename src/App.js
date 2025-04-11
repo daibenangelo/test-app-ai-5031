177de226b2b1f19edf85787c1348309d33f3c114
daibenangelo <daibenangelosanchez@gmail.com>
2025-04-11 08:46:53 +0800

    Revert "resetting"
    This reverts commit bad1ba4133dfaa747b72e53e7d0e111f28999d89.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Navbar from "./Components/Navbar";
+import About from "./Components/About";
 import Grades from "./Components/Grades";
+import Shop from "./Components/Shop";
+import Counter from "./Components/Counter";
+import Triangle from "./Components/Triangle";
 import Order from "./Components/Order";
 import Name from "./Components/Name";
+import Anim from "./Components/Anim";
 import Home from "./Components/Home";
 import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -21,12 +26,53 @@ function App() {
           </>
         }
       />
+
+      <Route
+        path="/about"
+        element={
+          <>
+            <Navbar />
+            <About />
+          </>
+        }
+      />
+
       <Route
         path="/grades"
         element={
           <>
             <Navbar />
             <Grades />
+          </>
+        }
+      />
+
+      <Route
+        path="/shop"
+        element={
+          <>
+            <Navbar />
+            <Shop />
+          </>
+        }
+      />
+
+      <Route
+        path="/counter"
+        element={
+          <>
+            <Navbar />
+            <Counter />
+          </>
+        }
+      />
+
+      <Route
+        path="/triangle"
+        element={
+          <>
+            <Navbar />
+            <Triangle />
           </>
         }
       />
@@ -47,6 +93,16 @@ function App() {
           <>
             <Navbar />
             <Name />
+          </>
+        }
+      />
+
+      <Route
+        path="/anim"
+        element={
+          <>
+            <Navbar />
+            <Anim />
           </>
         }
       />
---
+++ src/Components/Triangle.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Triangle extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Triangle;
